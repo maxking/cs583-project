b00e2df19c57ef6306d437bf0ebb505f286170bc
Abhilash Raj <raj.abhilash1@gmail.com>
2016-05-08 16:50:17 -0700

    add license, add more info in readme, remove old code

--- src/bot.hs
+++ src/bot.hs
@@ -3,17 +3,10 @@ import Data.Maybe
 import qualified Data.ByteString.Char8 as B
 import BotCommand --TODO there is a command type in SimpleIRC. Check
 
+-- | onMessage Event handler. According the RFC 2812 , PrivMsg is used to send
+--  message to a user, a channel and a user in the channel.
 onMessage :: EventFunc
-onMessage s m
-    | msg == toByteString "!Hi!" =  sendMsg s chan $ sendToSender "Hi there!"
-    | B.isPrefixOf (toByteString "!") msg = sendMsg s sender $ sendToSender (execute msg)
-    | otherwise = putStrLn $ show m
-    where chan = fromJust $ mChan m
-          sender = fromJust $ mNick m
-          sender' = B.unpack $ fromJust $ mNick m
-          msg = mMsg m
-          toByteString = B.pack
-          sendToSender m = toByteString $ sender' ++ ": " ++ m
+onMessage s m = undefined
 
 
 execute :: B.ByteString -> String
@@ -22,16 +15,57 @@ execute m = "Sorry, " ++ B.unpack m ++ "command not found!"
 events = [(Privmsg onMessage)]
 
 freenode = (mkDefaultConfig "irc.freenode.net" "hasbot")
-           {cChannels = ["##maxking"]
-           , cEvents = events
-           }
+           {cChannels = ["##maxking"],
+           cEvents = events}
+
 main = connect freenode False True
 
 
 process :: Command a -> String
 process (Cmd c f ss) = f (c ss)
 
---may be we can define a tiny weeny parser (using the parser combinator) to parse the input text into ChatMsg type?
-parse :: String -> ChatMsg a
-parse = undefined 
+-- may be we can define a tiny weeny parser (using the parser combinator) to
+-- parse the input text into ChatMsg type?
+-- parse also defined in simpleIRC
+-- parse :: String -> ChatMsg a
+-- parse = undefined
 -- parse error for undefined command
+
+
+
+-- | Define a test IRC message to the channel for testing
+msg :: IrcMessage
+msg = IrcMessage {mNick = Just $ B.pack "maxking",
+                  mUser = Just $ B.pack "~phoenix",
+                  mHost = Just $ B.pack "104.131.128.74",
+                  mServer = Nothing,
+                  mCode = B.pack "PRIVMSG",
+                  mMsg = B.pack "hi",
+                  mChan = Just $ B.pack "##maxking",
+                  mOrigin = Just $ B.pack "##maxking",
+                  mOther = Nothing,
+                  mRaw = B.pack ":maxking!~phoenix@104.131.128.74 PRIVMSG ##maxking :hi"}
+
+msg2 :: IrcMessage
+msg2 = IrcMessage {mNick = Just $ B.pack "maxking",
+                   mUser = Just $ B.pack "~phoenix",
+                   mHost = Just $ B.pack "104.131.128.74",
+                   mServer = Nothing,
+                   mCode = B.pack "PRIVMSG",
+                   mMsg = B.pack "hasbot: How you you man?",
+                   mChan = Just $ B.pack "##maxking",
+                   mOrigin = Just $ B.pack "##maxking",
+                   mOther = Nothing,
+                   mRaw = B.pack ":maxking!~phoenix@104.131.128.74 PRIVMSG ##maxking :hasbot: How you you man?"}
+
+msg3 :: IrcMessage
+msg3 = IrcMessage {mNick = Just $ B.pack "maxking",
+                   mUser = Just $ B.pack "~phoenix",
+                   mHost = Just $ B.pack "104.131.128.74",
+                   mServer = Nothing,
+                   mCode = B.pack "PRIVMSG",
+                   mMsg = B.pack "Hi Man!",
+                   mChan = Just $ B.pack "hasbot",
+                   mOrigin = Just $ B.pack "maxking",
+                   mOther = Nothing,
+                   mRaw = B.pack ":maxking!~phoenix@104.131.128.74 PRIVMSG hasbot :Hi Man!"}
