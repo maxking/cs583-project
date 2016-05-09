eb1b519914953b70f1e9f8cda58ced5c747708f4
deepthiskumar <deepthi.s.kumar8@gmail.com>
2016-05-08 23:19:28 -0700

    Parser, integrating it with the event handler

--- src/BotCommand.hs
+++ src/BotCommand.hs
@@ -1,6 +1,10 @@
 module BotCommand where
 
 import Data.ByteString
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Char
+import Text.Parsec.Pos
+import Text.ParserCombinators.Parsec.Error
 
 
 {-
@@ -9,17 +13,73 @@ If it is a command then it needs to be represented in a form where it is easier 
 A command takes 2 functions. One to transform the input string into required types and other the actual function that performs the operation.
 -}
 
-data Command a = Cmd ([String] -> a) (a -> String) [String]
+data Command = Command
+ { execute   :: [String] -> String,
+   args      :: [String]
+ }
+type CommandName = String
+type CommandMap = CommandName -> Maybe ([String] -> Command)
 
 type Message = String
 
-data ChatMsg a = Either Message (Command a)
+data ChatMsg a = Msg String | Cmd Command
 
-neg :: [String] -> Command Int
-neg = Cmd convertToInt (\i -> show (-i))
+------command getter and setter-------------------------------------------------------------
 
-add :: [String] -> Command (Int,Int)
-add = Cmd convertToIntInt (\(i,j) -> show (i+j))
+mempty :: CommandMap
+mempty = \_ -> Nothing
+
+set :: CommandName -> ([String] -> Command) -> CommandMap -> CommandMap
+set x i env = \y -> if x == y then (Just i) else env y
+
+get :: CommandName -> CommandMap -> Maybe ([String] -> Command)
+get x env = env x
+
+--Set Commands
+--TODO should we think of some otherway to map command name to command type??
+commands :: CommandMap
+commands = set "add" add (set "neg" neg mempty)
+
+------Parser--------------------------------------------------------------------------------
+
+parseCmd :: GenParser Char st (ChatMsg a)
+parseCmd = do
+    doc <- chars
+    eof
+    return $ (doc) 
+-- parse error for undefined command
+
+chars :: GenParser Char st (ChatMsg a)
+chars = do
+    try 
+     command <|> message
+
+command :: GenParser Char st (ChatMsg a)
+command = try $ do
+	       esclamation <- oneOf "-"
+	       command <- many alphaNum
+	       argString <- getInput
+	       case get command commands of
+	       	Nothing -> fail (command ++ " not found")
+	       	Just f  -> return $ Cmd (f (words argString))
+	       
+
+
+message :: GenParser Char st (ChatMsg a)
+message = do
+	       msg <- getInput
+	       return (Msg msg)
+
+
+
+------------custome commands------------------------------------------------------------------------
+
+
+neg :: [String] -> Command
+neg s = Command {execute = (\i -> show (-i)).convertToInt, args = s}
+
+add :: [String] -> Command
+add s = Command { execute = (\(i,j) -> show (i+j)).convertToIntInt, args = s}
 
 
 convertToInt :: [String] -> Int
--- src/bot.hs
+++ src/bot.hs
@@ -1,10 +1,20 @@
-import Network.SimpleIRC hiding (Command)
+--use -XBangPatterns while compiling
+
+import Network.SimpleIRC hiding (Command,parse)
 import Data.Maybe
 import qualified Data.ByteString.Char8 as B
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Char
 import BotCommand --TODO there is a command type in SimpleIRC. Check
 
+--EventFunc = MIrc -> IrcMessage -> IO ()
 onMessage :: EventFunc
-onMessage s m
+onMessage s m = case commandParser msg of
+                 Left error           -> putStrLn $ show error
+                 Right (Msg onlyMsg) -> putStrLn $ show onlyMsg
+                 Right (Cmd (cmd))  -> putStrLn $ show $ process cmd
+                where msg = B.unpack $ mMsg m
+{-onMessage s m
     | msg == toByteString "!Hi!" =  sendMsg s chan $ sendToSender "Hi there!"
     | B.isPrefixOf (toByteString "!") msg = sendMsg s sender $ sendToSender (execute msg)
     | otherwise = putStrLn $ show m
@@ -14,6 +24,14 @@ onMessage s m
           msg = mMsg m
           toByteString = B.pack
           sendToSender m = toByteString $ sender' ++ ": " ++ m
+
+-}
+
+test :: String -> IO ()
+test msg = case commandParser msg of
+                 Left error           -> putStrLn $ show error
+                 Right (Msg onlyMsg) -> putStrLn $ show onlyMsg
+                 Right (Cmd (cmd))  -> putStrLn $ show $ process cmd
 
 
 execute :: B.ByteString -> String
@@ -25,13 +43,16 @@ freenode = (mkDefaultConfig "irc.freenode.net" "hasbot")
            {cChannels = ["##maxking"]
            , cEvents = events
            }
-main = connect freenode False True
+--main = connect freenode False True
+{-main = do
+  line <- getLine
+  putStrLn ("input " ++ line)
+  test line
+-}
 
+process :: Command -> String
+process (Command f ss) = f ss
 
-process :: Command a -> String
-process (Cmd c f ss) = f (c ss)
+commandParser :: String -> Either ParseError (ChatMsg a)
+commandParser = parse parseCmd ""
 
---may be we can define a tiny weeny parser (using the parser combinator) to parse the input text into ChatMsg type?
-parse :: String -> ChatMsg a
-parse = undefined 
--- parse error for undefined command
