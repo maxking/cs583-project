470710c30eca39a627ab1756de106e0bc155932b
Abhilash Raj <raj.abhilash1@gmail.com>
2016-05-17 11:11:35 -0700

    fork server process in a seperate thread

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ import Bot
 import Config
 import Network.SimpleIRC hiding (Command, parse)
 import Webserver
+import Control.Concurrent
 import Network.Wai.Handler.Warp
 
 -- | The list of event and their handlers. We only focus on responding to
@@ -17,6 +18,6 @@ freenode = (mkDefaultConfig "irc.freenode.net" Config.botName)
 -- | The main event loop
 -- connect :: MIrc -> Bool (threaded) -> Bool (Debug Mode) -> IO (Either IOError MIrc)
 main = do
-  connect freenode True True
   putStrLn $ "Listening on http://localhost:" ++ show Config.port
-  run Config.port hasbotWebApp
+  forkIO $ run Config.port hasbotWebApp
+  connect freenode False True
